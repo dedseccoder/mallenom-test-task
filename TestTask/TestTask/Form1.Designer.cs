2c0f0b0b4689354083090bfd748aaf001eefe00e
dedseccoder <andrey.flach2@gmail.com>
2020-11-01 18:19:21 +0300

    minor changes

--- TestTask/TestTask/Form1.Designer.cs
+++ TestTask/TestTask/Form1.Designer.cs
@@ -37,11 +37,11 @@
             // RenderButton
             // 
             this.RenderButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.RenderButton.Location = new System.Drawing.Point(582, 338);
+            this.RenderButton.Location = new System.Drawing.Point(527, 342);
             this.RenderButton.Name = "RenderButton";
-            this.RenderButton.Size = new System.Drawing.Size(131, 40);
+            this.RenderButton.Size = new System.Drawing.Size(206, 39);
             this.RenderButton.TabIndex = 0;
-            this.RenderButton.Text = "Рендер";
+            this.RenderButton.Text = "Открыть изображение";
             this.RenderButton.UseVisualStyleBackColor = true;
             this.RenderButton.Click += new System.EventHandler(this.RenderButton_Click);
             // 
@@ -52,7 +52,7 @@
             this.Picture.Location = new System.Drawing.Point(50, 29);
             this.Picture.Name = "Picture";
             this.Picture.Size = new System.Drawing.Size(360, 270);
-            this.Picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Picture.TabIndex = 1;
             this.Picture.TabStop = false;
             this.Picture.Click += new System.EventHandler(this.Picture_Click);
@@ -68,7 +68,6 @@
             this.Text = "Test task";
             ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
