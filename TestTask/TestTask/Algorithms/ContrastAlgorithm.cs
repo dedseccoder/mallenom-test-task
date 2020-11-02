c9bc39473f5c72233f9c78beb16152286cd5d126
dedseccoder <andrey.flach2@gmail.com>
2020-11-02 23:55:51 +0300

    reworked ContrastAlgorith

--- TestTask/TestTask/Algorithms/ContrastAlgorithm.cs
+++ TestTask/TestTask/Algorithms/ContrastAlgorithm.cs
@@ -1,44 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
 
 namespace TestTask.Algorithms
 {
     class ContrastAlgorithm : Algorithm
     {
-        public override void MathPart(Bitmap input_picture)
+
+        public override Bitmap MathPart(Bitmap input_picture)
         {
-            var locked_bitmap = new BitmapLocker(input_picture);
-            locked_bitmap.LockBits();
+            int red, green, blue;
+            var contrast = Math.Pow((100.0 + 5000) / 100.0, 2);
 
-            var contrast = Math.Pow((100.0 + 50) / 100.0, 2);
-
-            for(int y = 0; y < locked_bitmap.Height; y++)
+            for(int y = 0; y < input_picture.Height; y++)
             {
-                for(int x = 0; x < locked_bitmap.Width; x++)
+                for(int x = 0; x < input_picture.Width; x++)
                 {
-                    var old_color = locked_bitmap.GetPixel(x, y);
-                    var red = ((((old_color.R / 255.0) - 0.5) * contrast) + 0.5 * 255.0);
-                    var green = ((((old_color.G / 255.0) - 0.5) * contrast) + 0.5 * 255.0);
-                    var blue = ((((old_color.B / 255.0) - 0.5) * contrast) + 0.5 * 255.0);
-                    
+                    var old_color = input_picture.GetPixel(x, y);
+                    double temp = ((((old_color.R / 255.0) - 0.5) * contrast) + 0.5 * 255.0);
+                    red = (int)temp;
+                    temp = ((((old_color.G / 255.0) - 0.5) * contrast) + 0.5 * 255.0);
+                    green = (int)temp;
+                    temp = ((((old_color.B / 255.0) - 0.5) * contrast) + 0.5 * 255.0);
+                    blue = (int)temp;
+
                     if(red > 255) red = 255;
-                    if(green > 255) green = 255;
-                    if(blue > 255) blue = 255;
-                    
                     if(red < 0) red = 0;
+
+                    if(green > 255) green = 255;
                     if(green < 0) green = 0;
+
+                    if(blue > 255) blue = 255;
                     if(blue < 0) blue = 0;
 
-                    var new_color = Color.FromArgb(old_color.A, (int) red, (int) green, (int) blue);
-                    locked_bitmap.SetPixel(x, y, new_color);
+                    input_picture.SetPixel(x, y, Color.FromArgb(old_color.A, red, green, blue));
                 }
             }
-            locked_bitmap.UnlockBits();
+            return input_picture;
         }
     }
 }
