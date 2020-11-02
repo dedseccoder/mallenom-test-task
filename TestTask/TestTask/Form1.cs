f361abf6353d249635320a0878ae6c3d15392f58
dedseccoder <andrey.flach2@gmail.com>
2020-11-03 00:00:12 +0300

    reworked button code

--- TestTask/TestTask/Form1.cs
+++ TestTask/TestTask/Form1.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Drawing;
 using System.Windows.Forms;
 using TestTask.Algorithms;
+using System.Dynamic;
 
 namespace TestTask
 {
@@ -12,26 +13,47 @@ namespace TestTask
         public Form1()
         {
             InitializeComponent();
+            LabelObj.Text = "";
+            ComboBoxObj.Items.AddRange(new string[] { "контраст", "градиент" });
         }
+        public Bitmap picture, rendered_picture;
         private void RenderButton_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "Image Files (*.BMP;*.PNG)|*.BMP;*.PNG|All files (*.*)|*.*";
+            ofd.Filter = "Image Files (*.BMP)|*.BMP|All files (*.*)|*.*";
 
-            if (ofd.ShowDialog() == DialogResult.OK)
+            if(ofd.ShowDialog() == DialogResult.OK)
             {
+              //  IRender renderer = new CanvasRender();
+                picture = new Bitmap (ofd.FileName);
                 Picture.Image = new Bitmap(ofd.FileName);
-                IRender renderer = new CanvasRender();
-                renderer.render(new Bitmap(ofd.FileName));
+
+                if(ComboBoxObj.SelectedItem != null)
+                {
+                    if(ComboBoxObj.SelectedItem.ToString() == "контраст")
+                    {
+                        ContrastAlgorithm contrast = new ContrastAlgorithm();
+                        RenderedPictureBox.Image = contrast.MathPart(new Bitmap(ofd.FileName));
+                    }
+                    else if(ComboBoxObj.SelectedItem.ToString() == "градиент")
+                    {
+                        GradientAlgorithm gradient = new GradientAlgorithm();
+                        gradient.grayscale = true;
+                        RenderedPictureBox.Image = gradient.MathPart(new Bitmap(ofd.FileName));
+                    }
+                }
+                else
+                {
+                    LabelObj.Text = "Что-то пошло не так";
+                }
             }
         }
-
         private void Picture_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        private void ComboBoxObj_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
